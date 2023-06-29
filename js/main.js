64e815c7db7af238dbccfd320804feeb27849f4f
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-06-29 18:00:52 -0400

    feat: create a features section
    - Add features section html markup and css styling
    - Add images in assets folder for features section
    - Add scroll animation for features section
    - Add min-width on html element to prevnt shrinking
    - Refactor renamed hero-download-button for consistency and changed width on hero-download-btn

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ function toggleMobileMenu() {
   function unToggleMobileMenu() {
     mobileMenuContainerEle.classList.toggle('js-show-menu-toggle');
 }
+
+
+
+// FEATURES SECTION SCROLL ANIMATION:
+const featuresContainer = document.querySelectorAll('.features-container__feature');
+
+// Use IntersectionObserver WebAPI to track wether or not the element is in viewport(window) if so add animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    (entry.isIntersecting) ? entry.target.classList.add('showFeature') : "";
+  })
+});
+// query all elements with hideFeature class 
+const hiddenElements = document.querySelectorAll('.hideFeature'); 
+// loop each element with class and set it to be observed
+hiddenElements.forEach((ele) => {
+  observer.observe(ele);
+})
