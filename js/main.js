dc01c5331f41472d57a84bd624d22bd166296819
[joao miguel] <83466001+joao-miguel8@users.noreply.github.com>
2023-07-02 19:14:52 -0400

    feat: create language selection drop down menu and social media container
    - Add html markup, css layout, css styling for language drop down menu
    - Add language drop down menu toggling functionality
    - Add html markup, css layout, css styling for social media socials container
    - Add social media icons in assets folder

--- js/main.js
+++ js/main.js
@@ -3,9 +3,18 @@ const mobileMenuBtn = document.querySelector('.menu');
 const mobileMenuContainerEle = document.querySelector('.nav-mobile__nav-mobile-links-container-wrapper');
 const mobileCloseMenuBtn = document.querySelector('.close-icon');
 
+// FEATURES SECTION SCROLL ANIMATION:
+const featuresContainer = document.querySelectorAll('.features-container__feature');
+
+// TOGGLE LANGUAGES CONTAINER
+const languageSelectedButton = document.querySelector('.selected-dropdown-lang');
+const languagesContainerEle = document.querySelector('.info-box-container__dropmenu');
+
+
 //<<<<<<<<< EVENT LISTENERS >>>>>>>
 mobileMenuBtn.addEventListener('click', toggleMobileMenu);
 mobileCloseMenuBtn.addEventListener('click', unToggleMobileMenu)
+languageSelectedButton.addEventListener('click', openLanguageMenu);
 
 function toggleMobileMenu() {
   mobileMenuContainerEle.classList.toggle('js-show-menu-toggle');
@@ -17,8 +26,6 @@ function toggleMobileMenu() {
 
 
 
-// FEATURES SECTION SCROLL ANIMATION:
-const featuresContainer = document.querySelectorAll('.features-container__feature');
 
 // Use IntersectionObserver WebAPI to track wether or not the element is in viewport(window) if so add animation
 const observer = new IntersectionObserver((entries) => {
@@ -33,4 +40,18 @@ hiddenElements.forEach((ele) => {
   observer.observe(ele);
 })
 
+
+
+
+// open and close language selection drop down menu
+function openLanguageMenu() {
+    languagesContainerEle.classList.toggle('languageMenuToggle');
+}
+
+// function to hide language drop down menu if user clicks on window
+window.onclick = function (event) { 
+  if(!event.target.matches(".selected-dropdown-lang")) {
+languagesContainerEle.classList.add('languageMenuToggle');
+  }
+}
 
